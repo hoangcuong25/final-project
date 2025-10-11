5432b8c97e36fc22d5b9a9dbf2a6d42244f55044
hoangcuong25 <vancuong25112003@gmail.com>
2025-10-11 11:48:08 +0700

    (FE) feat: middleware protect admin route

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// middleware.ts
+import { jwtVerify } from "jose";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const secret = new TextEncoder().encode(process.env.JWT_REFRESH_TOKEN_SECRET!);
+
+export async function middleware(request: NextRequest) {
+  const token = request.cookies.get("refresh_token")?.value;
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  try {
+    const { payload } = await jwtVerify(token, secret);
+    const role = payload.role;
+
+    // Kiểm tra role nếu truy cập /admin
+    if (request.nextUrl.pathname.startsWith("/admin") && role !== "ADMIN") {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    // Kiểm tra role nếu truy cập /host
+    if (request.nextUrl.pathname.startsWith("/host") && role !== "HOST") {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    return NextResponse.next();
+  } catch (err) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/admin/:path*", "/host/:path*"],
+};
